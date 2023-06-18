8808fba37d147ef3f300ebda57ed3d62c606978b
Kritos <kritos95@windowslive.com>
2023-06-18 15:59:04 +0300

    server parse request body and get data.

--- server/endpoint.php
+++ server/endpoint.php
@@ -6,5 +6,11 @@ header('Content-Type: application/json; charset=utf-8');
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == 'POST') {
 	$body = file_get_contents('php://input');
-	echo $body;
+	$data = json_decode($body);
+
+	// Access data with $data->numbers or $data->game
+	
+	// In order to send the repsonse json encoding is needed.
+	// To send only the number ex: echo json_encode($data->numbers);
+	echo json_encode($data);
 }
